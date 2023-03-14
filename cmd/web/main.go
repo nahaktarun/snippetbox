4541a3e96ba0f100c522298c274a86d84a4c72c8
nahaktarun <tarunnahak25@gmail.com>
2023-03-14 12:51:44 +0530

    added the template files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,14 @@
 package main
-import {
-	"log"
-	"net/http"
-}
-
-func main(){
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandlerFunc("/snippet/create",snippetCreate)
-
-	log.Print("Starting server on 4000")
-
-	err := http.ListenAndServe(":4000", mux)
-	log.Fata(err)
+import (
+"log"
+"net/http"
+)
+func main() {
+mux := http.NewServeMux()
+mux.HandleFunc("/", home)
+mux.HandleFunc("/snippet/view", snippetView)
+mux.HandleFunc("/snippet/create", snippetCreate)
+log.Print("Starting server on :4000")
+err := http.ListenAndServe(":4000", mux)
+log.Fatal(err)
 }
