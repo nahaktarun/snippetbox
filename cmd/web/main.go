ddbb9bf367f9587842def87354d8d6e0294fac56
nahaktarun <tarunnahak25@gmail.com>
2023-03-14 15:53:31 +0530

    added the flag variables

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,13 +1,22 @@
 package main
 import (
+	"flag"
 "log"
 "net/http"
 )
 func main() {
+	// flag variables
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
+	
 	mux := http.NewServeMux()
 	// Create a file server which serves files out of the "./ui/static" directory.
 	// Note that the path given to the http.Dir function is relative to the project
 	// directory root.
+
+	
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	// Use the mux.Handle() function to register the file server as the handler for
 	// all URL paths that start with "/static/". For matching paths, we strip the
@@ -17,7 +26,7 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s",*addr)
+	err := http.ListenAndServe(*addr, mux)
 log.Fatal(err)
 }
