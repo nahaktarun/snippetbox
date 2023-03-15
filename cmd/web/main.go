2a9a5f0b38bd39b2e5037fcac4347241a8a3cd8c
nahaktarun <tarunnahak25@gmail.com>
2023-03-15 18:00:53 +0530

    routes separated

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -33,23 +33,11 @@ func main() {
 		errorLog: errorLog,
 		infoLog: infoLog,
 	}
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	// Register the other application routes as normal.
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
-
+	
 	srv := &http.Server{
 		Addr: *addr,
 		ErrorLog: errorLog,
-		Handler: mux,
+		Handler: app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s",*addr)
