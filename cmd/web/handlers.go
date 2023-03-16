874e432e60621d85062e54f9054594df1c99e235
nahaktarun <tarunnahak25@gmail.com>
2023-03-16 20:52:36 +0530

    changes for the template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -48,48 +48,41 @@ func (app *application)home(w http.ResponseWriter, r *http.Request) {
 
 	// w.Write([]byte("Hello from Snippetbox"))
 }
-func (app *application)snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		// http.NotFound(w, r)
-		app.notFound(w)
+	app.notFound(w)
 	return
 	}
-
 	snippet, err := app.snippets.Get(id)
-
-	if err != nil{
-		if errors.Is(err, models.ErrNoRecords){
-			app.notFound(w)
-		}else{
-			app.serveError(w,err)
-		}
-		return
+	if err != nil {
+	if errors.Is(err, models.ErrNoRecords) {
+	app.notFound(w)
+	} else {
+	app.serveError(w, err)
 	}
-
-
-	// fmt.Fprintf(w, "%+v", snippet)
-
-
+	return
+	}
 	files := []string{
-		"./ui/html/base.tmpl",
-		"./ui/html/partials/nav.tmpl",
-		"./ui/html/pages/view.tmpl",
+	"./ui/html/base.tmpl",
+	"./ui/html/partials/nav.tmpl",
+	"./ui/html/pages/view.tmpl",
 	}
-
-	// parse the template files
 	ts, err := template.ParseFiles(files...)
-	if err != nil{
-		app.serveError(w, err)
-		return
+	if err != nil {
+	app.serveError(w, err)
+	return
 	}
-
-	err = ts.ExecuteTemplate(w, "base", snippet)
-	if err != nil{
-		app.serveError(w, err)
+	// Create an instance of a templateData struct holding the snippet data.
+	data := &templateData{
+	Snippet: snippet,
 	}
-
-}
+	// Pass in the templateData struct when executing the template.
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+	app.serveError(w, err)
+	}
+	}
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
