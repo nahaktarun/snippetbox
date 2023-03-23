5000ab23dd01b8d4d693f5f0b96932c67003c384
nahaktarun <tarunnahak25@gmail.com>
2023-03-23 16:30:16 +0530

    created validator helper

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,8 +10,10 @@ import (
 "snippetbox.tarunnahak.com/internal/models"
 
 "github.com/julienschmidt/httprouter"
-"strings"
-"unicode/utf8"
+// "strings"
+// "unicode/utf8"
+
+"snippetbox.tarunnahak.com/internal/validator"
 
 )
 func (app *application)home(w http.ResponseWriter, r *http.Request) {
@@ -127,7 +129,7 @@ type snippetCreateForm struct {
 	Title   string
 	Content string
 	Expires  int
-	FieldErrors map[string]string
+	validator.Validator
 }
 
 
@@ -151,41 +153,28 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		Title : r.PostForm.Get("title"),
 		Content : r.PostForm.Get("content"),
 		Expires : expires,
-		FieldErrors : map[string]string{},
 	}
 
 
 
-	if strings.TrimSpace(form.Title) == "" {
-		form.FieldErrors["title"] = "This field cannot not be blank"
-	}else if utf8.RuneCountInString(form.Title) > 100{
-		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
-	}
+	form.CheckField(validator.NotBlank(form.Title),"title", "This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title,100),"title", "This field cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+	form.CheckField(validator.PermittedInt(form.Expires,1,7,365), "expires", "This field must equal 1, 7 or 365")
 
-	// check that the content value isn't blank
-	if strings.TrimSpace(form.Content) == ""{
-		form.FieldErrors["content"] = "This field cannot be blank"
-	}
 
-	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
-		form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
-	}
-	if len(form.FieldErrors ) > 0{
-		
+	if !form.Valid(){
 		data := app.newTemplateData(r)
 		data.Form = form
 		app.render(w, http.StatusUnprocessableEntity, "create.tmpl",data)
 		return
 	}
 
+
 	id, err := app.snippets.Insert(form.Title, form.Content,form.Expires)
 	if err != nil{
 		app.serveError(w, err)
 		return
 	}
-
-
-
-	
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d",id),http.StatusSeeOther)
 }
